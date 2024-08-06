aa29514bb6efc0a4681cbc3d6937af157bf6b006
Maya Kim <mayagy.kim@gmail.com>
2024-08-06 16:05:01 +0200

    created profile container on user show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :users, only: %i[show]
+  resources :users, only: %i[show edit update]
   resources :tasks, only: %i[create update delete]
 end
