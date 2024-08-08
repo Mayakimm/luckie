7900b6c6018a78a3dcb4fb279d3ee3108dd0680b
Maya Kim <mayagy.kim@gmail.com>
2024-08-08 22:27:04 +0200

    created weekly calendar frame

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,25 @@ class TasksController < ApplicationController
 
   def delete
   end
+
+  def schedule
+    @start_time = '08:00'
+    @end_time = '18:00'
+    @time_slot_duration = 1.hour
+    @time_slots = generate_time_slots(@start_time, @end_time, @time_slot_duration)
+  end
+
+  private
+
+  def generate_time_slots(start_time, end_time, duration)
+    time_slots = []
+    start_time = Time.parse(start_time)
+    end_time = Time.parse(end_time)
+    while start_time < end_time
+      time_slots << start_time
+      start_time += duration
+    end
+
+    time_slots
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :users, only: %i[show edit update]
-  resources :tasks, only: %i[create update delete]
+  resources :tasks, only: %i[create update delete] do
+    collection do
+      get :schedule
+    end
+  end
 end
